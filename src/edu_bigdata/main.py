1000bffea355f0a2e787d91bbfc5b019d81aae4f
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-05-01 00:59:21 +0000

    creacion de la clase database

--- src/edu_bigdata/main.py
+++ src/edu_bigdata/main.py
@@ -1,14 +1,16 @@
 from dataweb import DataWeb
+from database import DataBase
 import pandas as pd
 
 
 
 def main():
     dataweb = DataWeb()
+    database = DataBase()
     df = dataweb.obtener_datos()
     df = dataweb.convertir_numericos(df)
     df.to_csv("src/edu_bigdata/static/csv/data_web.csv", index=False) #/workspaces/bigdata_2025_1_2/src/edu_bigdata/static/csv
-
+    database.close_database()
 
 
 if __name__ == "__main__":
